d59b1737bad2af0af399d5db0b964aaf33e6aece
denver-code <csigorek@gmail.com>
2022-11-23 18:07:07 +0000

    temp fix for startup after clear install

--- dsk/ini/boot.sh
+++ dsk/ini/boot.sh
@@ -1,5 +1,5 @@
-vga set palette /ini/palette.csv
-vga set font /ini/fonts/zap-light-8x16.psf
-cat /ini/banner.txt
-user login
+vga set palette /ini/palette.csv 
+vga set font /ini/fonts/zap-light-8x16.psf 
+cat /ini/banner.txt 
+user login 
 shell 
